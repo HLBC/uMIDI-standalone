76bb46a0ab84f2bceb90d012aae459f3f795c79f
Robbie Dozier <importantigrav@gmail.com>
2020-03-04 21:21:21 -0500

    Implemented most of Note class, Tonic enum, MidiMessage struct

--- uMIDI/uMIDI/Common/Midi.cs
+++ uMIDI/uMIDI/Common/Midi.cs
@@ -3,25 +3,12 @@ namespace uMIDI.Common
     public struct MidiMessage
     {
         public byte Status;
-        public byte Byte1;
-        public byte Byte2;
+        public byte MsgSize;
+        public byte[] Data;
     }
 
     public interface IMessage
     {
         MidiMessage Message { get; };
-    }
-
-    public interface INote
-    {
-        INote ShiftByInterval(int interval);
-    }
-
-    public class MSystemReset : IMessage
-    {
-        public override MidiMessage Message
-        {
-            get { return new MidiMessage(Status = 0xFF, Byte1 = 0, Byte2 = 0); }
-        }
     }
 }
