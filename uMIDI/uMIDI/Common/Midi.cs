696fe5dcae820eb004d581f16f0b5af00f27b649
Robbie Dozier <importantigrav@gmail.com>
2020-03-04 21:24:55 -0500

    Implemented NoteOnMessage

--- uMIDI/uMIDI/Common/Midi.cs
+++ uMIDI/uMIDI/Common/Midi.cs
@@ -11,4 +11,17 @@ namespace uMIDI.Common
     {
         MidiMessage Message { get; };
     }
+
+    public class NoteOnMessage : IMessage, Note
+    {
+        MidiMessage Message
+        {
+            get
+            {
+                byte status = 0x90 + Channel;
+                byte msgSize = 2;
+                return new MidiMessage(Status = status, MsgSize = msgSize, Data = new List<Byte> {Pitch, Velocity});
+            }
+        }
+    }
 }
