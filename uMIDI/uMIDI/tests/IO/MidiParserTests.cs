f8ab07b57f6aa91e189f0ab50c77a86720472d3e
johnk <johnkwak7@gmail.com>
2020-04-24 18:35:35 -0500

    added test file for midiparser

---
+++ uMIDI/uMIDI/tests/IO/MidiParserTests.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace uMIDI.tests.IO
+{
+    [TestClass()]
+    public class MidiParserTests
+    {
+    }
+}
