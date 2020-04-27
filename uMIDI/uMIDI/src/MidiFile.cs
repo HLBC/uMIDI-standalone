28f13960ef0e0610fbdee483cde3e45778f33104
Robbie Dozier <importantigrav@gmail.com>
2020-04-27 13:34:08 -0500

    Implemented PushBuffer() in MidiFile

--- uMIDI/uMIDI/src/MidiFile.cs
+++ uMIDI/uMIDI/src/MidiFile.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using uMIDI.Common;
 using uMIDI.IO;
 
@@ -9,22 +8,29 @@ namespace uMIDI
     {
         public MetaMidiStream MetaStream { get; }
         public int BufferSize { get; }
-        public bool EndOfFile { get; }
-        private List<AbstractMessage> _messages;
+        public bool EndOfFile { get; private set; }
+        public int MessageCount { get => _messages.Count; }
+        private List<IMessage> _messages;
         private int _playHead;
 
-        public MidiFile(MetaMidiStream stream, List<AbstractMessage> messages,
+        // Re-use the same memory for performance
+        private IMessage[] _buffer;
+        private IMessage[] _lastbuffer;
+
+        public MidiFile(MetaMidiStream stream, List<IMessage> messages,
             int bufferSize)
         {
             MetaStream = stream;
             BufferSize = bufferSize;
             EndOfFile = false;
-            _messages = new List<AbstractMessage>();
+            _messages = messages;
             _playHead = 0;
+            _buffer = new IMessage[BufferSize];
+            _lastbuffer = new IMessage[MessageCount % BufferSize];
         }
 
         public MidiFile(int ticksPerBeat, int bufferSize, int beatsPerMeasure,
-            int subdivision, int bpm, List<AbstractMessage> messages,
+            int subdivision, int bpm, List<IMessage> messages,
             KeySignature key) :
             this(new MetaMidiStream(ticksPerBeat, beatsPerMeasure, subdivision,
                     bpm, key), messages, bufferSize)
@@ -32,19 +38,47 @@ namespace uMIDI
 
         }
 
-        public void AddMessage(AbstractMessage messageData)
+        public void AddMessage(IMessage messageData)
         {
             _messages.Add(messageData);
+            _lastbuffer = new IMessage[MessageCount % BufferSize];
         }
 
-        public void AddMessage(List<AbstractMessage> messageData)
+        public void AddMessage(List<IMessage> messageData)
         {
             _messages.AddRange(messageData);
+            _lastbuffer = new IMessage[MessageCount % BufferSize];
+        }
+
+        public void Reset()
+        {
+            _playHead = 0;
+            _lastbuffer = new IMessage[MessageCount % BufferSize];
         }
 
         public void PushNextBuffer()
         {
+            IMessage[] buffer;
+            int bufferSize;
+            if (_playHead + BufferSize >= MessageCount)
+            {
+                buffer = _lastbuffer;
+                bufferSize = _lastbuffer.Length;
+                EndOfFile = true;
+            }
+            else
+            {
+                buffer = _buffer;
+                bufferSize = BufferSize;
+            }
 
+            for (int i = 0; i < bufferSize; i++)
+            {
+                buffer[i] = _messages[_playHead + i];
+            }
+            _playHead += bufferSize;
+
+            MetaStream.PushBuffer(buffer);
         }
     }
 }
